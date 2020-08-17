ec2091c46b23256d036aaf4fbf092fea4e3fa13b
mansourSaleh <mansouralmohsen2018@gmai.com>
2020-08-17 09:02:36 +0300

    aded

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ tabs.forEach(tab => tab.addEventListener('click',(e) => {
     tabs.forEach(t => t.classList.remove('active'))
    tab.classList.add('active')
 }))
+
+const lis = document.querySelectorAll('#li-s-active li')
+
+lis.forEach(li => li.addEventListener('click',(e) => {
+    lis.forEach(t => t.classList.remove('active'))
+   li.classList.add('active')
+}))
+
+const list = document.querySelectorAll('#li-curses li')
+
+list.forEach(lii => lii.addEventListener('click',(e) => {
+    list.forEach(t => t.classList.remove('active'))
+   lii.classList.add('active')
+}))
